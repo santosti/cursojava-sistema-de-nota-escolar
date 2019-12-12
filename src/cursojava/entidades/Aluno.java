3322d4646a2ea53b4f80f5a61c1d4e472a8b6ea6
Simone Antunes <sanntosti@gmail.com>
2019-12-12 13:35:37 -0300

    Primeiro commit

---
+++ src/cursojava/entidades/Aluno.java
@@ -0,0 +1,134 @@
+package cursojava.entidades;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Aluno {
+	public String nome;
+	public int idade;
+	private String dataNascimento;
+	private double nota;
+
+	private List<Disciplina> disciplinas = new ArrayList<>();
+
+	public Aluno() {
+	}
+
+	public Aluno(String nome, int idade, String dataNascimento) {
+		super();
+		this.nome = nome;
+		this.setIdade(idade);
+		this.dataNascimento = dataNascimento;
+	}
+
+	public List<Disciplina> getDisciplinas() {
+		return disciplinas;
+	}
+
+	public void setDisciplina(List<Disciplina> disciplinas) {
+		this.disciplinas = disciplinas;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public double getNota() {
+		return nota;
+	}
+
+	public void setNota(double nota) {
+		this.nota = nota;
+	}
+
+	//Percorre a lista de objetos, soma a nota das materias e atribui a variavel somaNota.
+	public double getMediaAluno() {
+		double somaNotas = 0.0;
+		for (Disciplina disc : disciplinas) {
+			somaNotas += disc.getNota();
+		}
+		return somaNotas/ disciplinas.size(); // size() retorna a quantidade existente dinamicamente.
+	}
+	
+	//Condição de aprovado e reprovado.
+	public boolean getResultado() {
+		double media = this.getMediaAluno();
+
+		if (media >= 70) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "Aluno [nome=" + nome + ", idade=" + idade + ", dataNascimento=" + dataNascimento + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((dataNascimento == null) ? 0 : dataNascimento.hashCode());
+		result = prime * result + ((disciplinas == null) ? 0 : disciplinas.hashCode());
+		result = prime * result + idade;
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(nota);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Aluno other = (Aluno) obj;
+		if (dataNascimento == null) {
+			if (other.dataNascimento != null)
+				return false;
+		} else if (!dataNascimento.equals(other.dataNascimento))
+			return false;
+		if (disciplinas == null) {
+			if (other.disciplinas != null)
+				return false;
+		} else if (!disciplinas.equals(other.disciplinas))
+			return false;
+		if (idade != other.idade)
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (Double.doubleToLongBits(nota) != Double.doubleToLongBits(other.nota))
+			return false;
+		return true;
+	}
+
+}
---
+++ src/cursojava/executavel/Aplicacao.java
@@ -0,0 +1,39 @@
+package cursojava.executavel;
+
+import javax.swing.JOptionPane;
+
+import cursojava.entidades.Aluno;
+import cursojava.entidades.Disciplina;
+
+public class Aplicacao {
+
+	public static void main(String[] args) {
+
+		Aluno aluno = new Aluno();
+
+		String nome = JOptionPane.showInputDialog("Informe o nome: ");
+		String idade = JOptionPane.showInputDialog("Informe a idade: ");
+		String dataNascimento = JOptionPane.showInputDialog("Informe a data de nascimento: ");
+
+		aluno.setNome(nome);
+		aluno.setIdade(Integer.valueOf(idade));
+		aluno.setDataNascimento(dataNascimento);
+
+		for (int i = 1; i <= 4; i++) {
+			String nomeDisciplina = JOptionPane.showInputDialog("Informe a disciplina " + i + ": ");
+			String notaDisciplina = JOptionPane.showInputDialog("Informe a nota " + i + ": ");
+			Disciplina disciplina = new Disciplina();
+			disciplina.setDisciplinas(nomeDisciplina);
+			disciplina.setNota(Double.valueOf(notaDisciplina));
+
+			aluno.getDisciplinas().add(disciplina);
+
+		}
+
+		System.out.println(aluno.toString());
+		System.out.println("Média do Aluno: " + aluno.getMediaAluno());
+		System.out.println("Resultado: " + (aluno.getResultado() ? "Aluno aprovado" : "Aluno reprovado"));
+
+	}
+
+}
